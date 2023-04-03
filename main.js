cbd1af8d9e2467e935f7972805cac7e1a7a4f0a4
Jason <jasonee96@gmail.com>
2023-04-03 13:40:29 -0500

    Added submit button functionality (check whether user selected a rating).

--- main.js
+++ main.js
@@ -3,6 +3,10 @@ Rating Button Click Functionality
     - If clicked, change color by adding class "rating-btn--clicked".
     - Save rating value to session storage
 */
+
+localStorage.clear('ratingValue'); // clears previous ratingValue whenever refreshed
+let ratingValue;
+
 let ratingBtn = $('.rating-btn');
 ratingBtn.on('click', function (e) {
     /* Styling */
@@ -10,7 +14,6 @@ ratingBtn.on('click', function (e) {
     e.currentTarget.classList.add('rating-btn--clicked');
     
     /* Get rating value*/
-    let ratingValue;
     if (e.target.value) { // if input.radio-btn is clicked (value)
     ratingValue = e.target.value;
     } else { // if div.rating-btn is clicked (data-value)
@@ -19,4 +22,15 @@ ratingBtn.on('click', function (e) {
 
     /* Save ratingValue to localStorage */
     localStorage.setItem('ratingValue', ratingValue);
+})
+
+/*
+Submit Button Functionality
+    - If ratingValue is empty, alert user.
+// */
+$('#submit-btn').on("click", function(e){
+    if (!ratingValue) {
+        e.preventDefault(); // stops page from refreshing/submitting
+        alert("Please select a rating.");
+    }
 })
